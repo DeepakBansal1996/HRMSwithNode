396dc443a4d3ca3e6c6173f75dfd8f3417914f68
Bansal <Deepak.Bansal@cybergindia.local>
2019-03-13 12:57:27 +0530

    added gitignore commit

--- app.js
+++ app.js
@@ -319,7 +319,7 @@ app.put('/adminhomepage/assignusertoproject/:id', (req, res) => {
 
 });
 //ser starting
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log("server started on the port " + port);
 });
